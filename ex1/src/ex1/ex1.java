8565f9d3b0b314f82cbc5644c67651ef5dfa038e
hellokikiqiqi <1792106897@qq.com>
2021-12-31 20:01:18 +0800

    ex1

---
+++ ex1/src/ex1/ex1.java
@@ -0,0 +1,32 @@
+package ex1;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Display message Welcome to Java! on the console
+		System.out.println("Welcome to Java!");
+		System.out.println("Programming is fun!");
+		System.out.println("Fundamentals First");
+		System.out.println("Problem Driven");
+		System.out.println("(10.5 + 2 * 3) / (45 - 3.5) = ");
+		System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+	}
+	
+//	//error
+//	public static main(String[] args) {
+//		// TODO Auto-generated method stub
+//		
+//		//void main, " "
+//		System.out.println("Welcome to Java!);"
+//		
+//		// 0
+//		System.out.println(1 / 0);
+//		
+//		//9 / 5 = 1
+//		System.out.println("Calsius 35 is Fahrenheit degree ");
+//		System.out.println((9 / 5) * 35 + 32);
+//	}		
+
+}
